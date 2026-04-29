d12ab8a48b6fcea0459824095014cb9d59a8b953
PeachCellWasTaken <user@example.com>
2026-04-28 22:29:38 -0400

    updated spam to s command - accepts custom message and amount

--- commands/raiding/spam.js
+++ commands/raiding/spam.js
@@ -1,44 +1,24 @@
 module.exports = {
-  name: 'spam',
-  description: 'Spam messages',
+  name: 's',
+  description: 'Spam custom message',
   execute: async (msg, args, client) => {
-    if (!args.length || !msg.guild) return;
+    if (args.length < 2 || !msg.guild) return;
     
-    const raidMsg = `# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+    const customMsg = args.slice(0, -1).join(' ');
+    const count = parseInt(args[args.length - 1]) || 1;
+    const channels = msg.guild.channels.cache.filter(ch => ch.isTextBased());
+    
+    try {
+      for (const [, channel] of channels) {
+        for (let i = 0; i < count; i++) {
+          try {
+            await channel.send(customMsg);
+          } catch (e) {}
+        }
+      }
+    } catch (e) {}
+  }
+};
 # R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL`;
 
     const count = parseInt(args[0]) || 100;
