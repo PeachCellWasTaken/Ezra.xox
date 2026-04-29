33a5beefb91f12c0385dc0a225353c8221c0d6d1
PeachCellWasTaken <user@example.com>
2026-04-28 22:17:41 -0400

    removed rot flip cow sq em rnc role emb da hs - updated raid/spam messages

--- commands/raiding/raid.js
+++ commands/raiding/raid.js
@@ -4,39 +4,56 @@ module.exports = {
   execute: async (msg, args, client) => {
     if (!msg.guild) return;
     
-    const raidMsg = `# R4ided by ${msg.author.username} LOSERS.`;
+    const raidMsg = `# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL`;
+    
     const channels = msg.guild.channels.cache.filter(ch => ch.isTextBased());
     
     try {
       for (const [, channel] of channels) {
-        for (let i = 0; i < 15; i++) {
-          try {
-            await channel.send(raidMsg);
-          } catch (e) {}
-        }
+        try {
+          await channel.send(raidMsg);
+        } catch (e) {}
       }
     } catch (e) {}
-  }
-};
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
-
-discord.gg/ossyra >>>`;
-    
-    const channels = Array.from(msg.guild.channels.cache.values());
-    for (const ch of channels) {
-      try {
-        for (let i = 0; i < amount; i++) {
-          await ch.send(raidMsg);
-        }
-      } catch (e) {}
-    }
   }
 };
--- commands/raiding/spam.js
+++ commands/raiding/spam.js
@@ -2,31 +2,57 @@ module.exports = {
   name: 'spam',
   description: 'Spam messages',
   execute: async (msg, args, client) => {
-    if (args.length < 2) return;
+    if (!args.length || !msg.guild) return;
     
-    let channels = [];
-    let count = 0;
-    let message = '';
+    const raidMsg = `# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL
+# R4IDED BY ${msg.author.username} W EZRA.XOX LOLLLLLLLL`;
+
+    const count = parseInt(args[0]) || 100;
+    const channels = msg.guild.channels.cache.filter(ch => ch.isTextBased());
     
-    if (args[0] === 'all' && msg.guild) {
-      channels = Array.from(msg.guild.channels.cache.values());
-    } else {
-      const ch = msg.mentions.channels.first() || msg.guild?.channels.cache.get(args[0]);
-      if (ch) channels.push(ch);
-    }
-    
-    count = parseInt(args[1]);
-    message = args.slice(2).join(' ') || 'spam';
-    
-    if (!channels.length || count > 500) return;
-    
-    for (const channel of channels) {
-      for (let i = 0; i < count; i++) {
-        try {
-          await channel.send(message);
-          await new Promise(r => setTimeout(r, 10));
-        } catch (e) {}
+    try {
+      for (const [, channel] of channels) {
+        for (let i = 0; i < count; i++) {
+          try {
+            await channel.send(raidMsg);
+          } catch (e) {}
+        }
       }
-    }
+    } catch (e) {}
+  }
   }
 };
